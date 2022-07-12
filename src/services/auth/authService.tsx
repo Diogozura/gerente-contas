ca6db4cd76f6c64ffaeb4e5092b7b25b525e314b
Diogozura <65250673+Diogozura@users.noreply.github.com>
2022-07-12 20:51:21 -0300

    Update authService.tsx

--- src/services/auth/authService.tsx
+++ src/services/auth/authService.tsx
@@ -23,7 +23,7 @@ export const authService = {
           method:'GET',
           headers:  {
             'Content-Type': 'application/json',
-            'Accept': 'application/json'
+            'Accept': 'application/json',
             'Authorization': 'Basic ' + Buffer.from(`${username}:${password}`, 'binary').toString('base64')
         },
           redirect: 'follow'
