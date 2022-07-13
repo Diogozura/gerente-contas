7890a0a9b23bd464508a2f75523c08544a6d1c4d
Diogozura <65250673+Diogozura@users.noreply.github.com>
2022-07-12 23:10:40 -0300

    Update authService.tsx

--- src/services/auth/authService.tsx
+++ src/services/auth/authService.tsx
@@ -19,7 +19,7 @@ export const authService = {
         // var myHeaders = new Headers();
         // myHeaders.append('Authorization', 'Basic ' + Buffer.from(`${username}:${password}`, 'binary').toString('base64'))
     
-        await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}api/login`, {
+        await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/login`, {
           method:'GET',
           headers:  {
             'Content-Type': 'application/json',
@@ -30,7 +30,7 @@ export const authService = {
         })
         .then(async(response)=> {
             response.text().then(async(data)=> {
-                console.log(data);
+                
                 // tokenService.save(data.token);
             });
           }).catch((err)=> {
