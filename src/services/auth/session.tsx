6995509109453ad63460b5aecf35dfb921c0ebe9
Diogozura <65250673+Diogozura@users.noreply.github.com>
2022-07-19 20:23:19 -0300

    .

--- src/services/auth/session.tsx
+++ src/services/auth/session.tsx
@@ -14,7 +14,7 @@ export function withSession(fucao) {
      }
       return fucao(modifiedCtx)
 
-    }catch (err) {
+    }catch(err){
       return {
               redirect: {
                 permanent: false,
